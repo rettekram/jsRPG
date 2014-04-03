8bb6b1bb962ce0270ac67d93d88c8a560b639206
Mark Etter <metter@Black-and-Tan.local>
2014-04-02 17:08:13 -0700

    Setup the inital methods for the Character class

--- core/character.js
+++ core/character.js
@@ -1,6 +1,6 @@
 /**
  * @file
- * Controls the encounter rounds and actions.
+ * Controls the character creation.
  * 
  */
 
@@ -13,8 +13,8 @@
  */
 Character = function() {
 	// sets default parameters
-	this.currentHP = 0;
-	this.maxHP = 0;
+	this.maxHP = 4;
+	this.currentHP = this.maxHP;
 	this.coreClass = [];
 
 	// debug to the screen
@@ -23,19 +23,62 @@ Character = function() {
 
 
 /**
- * Iterates through all the players in the combat encounter and checks the initative order
- * @todo
+ * Getter method the Maximum Hit Point property.
+ * 
  */
-Character.prototype.setHP = function () {
-	// debug to the screen
-	if (this.debug) {
-		// log the results
-		console.log (this);
+Character.prototype.setMaxHP = function (newMaxHP) {
+	// validates as integer
+	if (typeof newMaxHP !== 'undefined') {
+		newMaxHP = parseInt(newMaxHP, 10);
 	}
+	
+	// update  value
+	this.maxHP = newMaxHP;
+	
+	// returns the value
+	return this.maxHP;
 };
 
 
-var Mark = new Character();
-console.log (Mark);
+/**
+ * Setter method the Maximum Hit Point property.
+ * 
+ */
+Character.prototype.getMaxHP = function () {
+	// returns the value
+	return this.maxHP;
+};
+
+
+/**
+ * Adds a dice rolled value to the Maximum Hit Point property.
+ * 
+ */
+Character.prototype.rollMaxHP = function (diceRoll) {
+	// save new dice roll to a variable
+	var rollHP = new Dice,
+			addHP = rollHP.parseDice(diceRoll),
+			arrayHP = [this.maxHP, addHP],
+			newMaxHP;
+
+	// adds array values together
+	newMaxHP = arrayHP.sum();
+	
+	// new value becomes the maxHP 
+	this.maxHP = newMaxHP;
+	
+	// return the value
+	return this.maxHP;
+};
+
+
+var Bob = new Character();
+Bob.setMaxHP(8);
+console.log("Bob's maximum HP is", Bob.getMaxHP());
+console.log("Bob gains a level!");
+Bob.rollMaxHP("1d6+2");
+console.log("-----------");
+console.log("Bob's new maximum HP is", Bob.getMaxHP());
+
 
 }(jsRPG));
--- test/character.tests.js
+++ test/character.tests.js
@@ -4,9 +4,27 @@
  * 
  */
 
-module("Character");
+// Define test variables
+module("Character", {
+	setup: function() {
+		var maxHP = "8",
+				currentHP = 8,
+				hitDice = "1d6+2";
+	}
+});
 
-// Test methods existance
+// Test method existance
 test("All required methods exist", function() {
   ok(Character, "Character constructor method exists");
+  ok(Character.prototype.setMaxHP, "setMaxHP method exists");
+  ok(Character.prototype.getMaxHP, "getMaxHP method exists");
+  ok(Character.prototype.rollMaxHP, "rollMaxHP method exists");
+});
+
+// Test that methods are providing the correct values
+test("All methods provide the correct type of value", function() {
+	strictEqual($.type(Character), "function", "Character constructor result is a function");
+  strictEqual($.type(Character.prototype.setMaxHP(10)), "number", "setMaxHP result is a number");
+  strictEqual($.type(Character.prototype.getMaxHP()), "number", "getMaxHP result is a number");
+  strictEqual($.type(Character.prototype.rollMaxHP("1d6+2")), "number", "rollMaxHP result is a number");
 });
