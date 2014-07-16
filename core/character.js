19ec3ca4a7101c39e7fdaabbab1f7547c0d229a8
rettekram <mark.etter@gmail.com>
2014-07-16 16:52:14 -0700

    Include basic methods and unit tests for character class.

--- core/character.js
+++ core/character.js
@@ -13,9 +13,9 @@
  */
 Character = function() {
 	// sets default parameters
-	this.maxHP = 4;
-	this.currentHP = this.maxHP;
-	this.coreClass = [];
+	this.hp = 0;
+	this.maxHP = 0;
+	this.level = 0;
 
 	// debug to the screen
 	this.debug = true;
@@ -23,9 +23,43 @@ Character = function() {
 
 
 /**
- * Getter method the Maximum Hit Point property.
+ * Getter/Setter methods the Hit Point property.
  * 
  */
+Character.prototype.getHP = function () {
+	// validates as integer
+	this.hp = parseInt(this.hp, 10);
+	
+	// returns the value
+	return this.hp;
+};
+
+Character.prototype.setHP = function (newHP) {
+	// validates as integer
+	if (typeof newHP !== 'undefined') {
+		newHP = parseInt(newHP, 10);
+	}
+	
+	// update value
+	this.hp = newHP;
+	
+	// returns the value
+	return this.hp;
+};
+
+
+/**
+ * Getter/Setter methods the Maximum Hit Point property.
+ * 
+ */
+Character.prototype.getMaxHP = function () {
+	// validates as integer
+	this.maxHP = parseInt(this.maxHP, 10);
+	
+	// returns the value
+	return this.maxHP;
+};
+
 Character.prototype.setMaxHP = function (newMaxHP) {
 	// validates as integer
 	if (typeof newMaxHP !== 'undefined') {
@@ -35,16 +69,6 @@ Character.prototype.setMaxHP = function (newMaxHP) {
 	// update  value
 	this.maxHP = newMaxHP;
 	
-	// returns the value
-	return this.maxHP;
-};
-
-
-/**
- * Setter method the Maximum Hit Point property.
- * 
- */
-Character.prototype.getMaxHP = function () {
 	// returns the value
 	return this.maxHP;
 };
@@ -63,22 +87,59 @@ Character.prototype.rollMaxHP = function (diceRoll) {
 
 	// adds array values together
 	newMaxHP = arrayHP.sum();
-	
+
 	// new value becomes the maxHP 
-	this.maxHP = newMaxHP;
-	
+	this.setMaxHP(newMaxHP);
+
 	// return the value
 	return this.maxHP;
 };
 
 
+/**
+ * Getter/Setter methods the Character Level property.
+ * 
+ */
+Character.prototype.getLevel = function () {
+	// validates as integer
+	this.level = parseInt(this.level, 10);
+	
+	// returns the value
+	return this.level;
+};
+
+Character.prototype.setLevel = function (newLevel) {
+	// validates as integer
+	if (typeof newLevel !== 'undefined') {
+		newLevel = parseInt(newLevel, 10);
+	}
+	
+	// update  value
+	this.level = newLevel;
+	
+	// returns the value
+	return this.level;
+};
+
+
+
+
+
+
+
+
+/**
+ * Debug Output
+ * 
+ */
 var Bob = new Character();
-Bob.setMaxHP(8);
-console.log("Bob's maximum HP is", Bob.getMaxHP());
-console.log("Bob gains a level!");
-Bob.rollMaxHP("1d6+2");
-console.log("-----------");
-console.log("Bob's new maximum HP is", Bob.getMaxHP());
+if (Bob.debug == 1) {
+	Bob.setMaxHP(12);
+	console.log("Bob's maximum HP is", Bob.getMaxHP());
+	console.log("Bob gains a level!");
+	Bob.rollMaxHP("1d10+2");
+	console.log("Bob's new maximum HP is", Bob.getMaxHP());
+}
 
 
 }(jsRPG));
--- test/character.tests.js
+++ test/character.tests.js
@@ -4,9 +4,29 @@
  * 
  */
 
-// Define test variables
-module("Character", {
-	setup: function() {
+/**
+ * Character tests
+ * 
+ */
+module("Character");
+
+// Test method existance
+test("All required methods exist", function() {
+  ok(Character, "Character constructor method exists");
+});
+
+// Test that methods are providing the correct values
+test("All methods provide the correct type of value", function() {
+	strictEqual($.type(Character), "function", "Character constructor result is a function");
+});
+
+
+/**
+ * Hit Point tests
+ * 
+ */
+module("Hit Points", {
+	setup: function () {
 		var maxHP = "8",
 				currentHP = 8,
 				hitDice = "1d6+2";
@@ -15,16 +35,37 @@ module("Character", {
 
 // Test method existance
 test("All required methods exist", function() {
-  ok(Character, "Character constructor method exists");
-  ok(Character.prototype.setMaxHP, "setMaxHP method exists");
+  ok(Character.prototype.getHP, "getHP method exists");
+  ok(Character.prototype.setHP, "setHP method exists");
   ok(Character.prototype.getMaxHP, "getMaxHP method exists");
+  ok(Character.prototype.setMaxHP, "setMaxHP method exists");
   ok(Character.prototype.rollMaxHP, "rollMaxHP method exists");
 });
 
 // Test that methods are providing the correct values
 test("All methods provide the correct type of value", function() {
-	strictEqual($.type(Character), "function", "Character constructor result is a function");
-  strictEqual($.type(Character.prototype.setMaxHP(10)), "number", "setMaxHP result is a number");
+  strictEqual($.type(Character.prototype.getHP()), "number", "getHP result is a number");
+  strictEqual($.type(Character.prototype.setHP(10)), "number", "setHP result is a number");
   strictEqual($.type(Character.prototype.getMaxHP()), "number", "getMaxHP result is a number");
-  strictEqual($.type(Character.prototype.rollMaxHP("1d6+2")), "number", "rollMaxHP result is a number");
+  strictEqual($.type(Character.prototype.setMaxHP(10)), "number", "setMaxHP result is a number");
+  strictEqual($.type(Character.prototype.rollMaxHP("1d6+1")), "number", "rollMaxHP result is a number");
+});
+
+
+/**
+ * Hit Point tests
+ * 
+ */
+module("Level");
+
+// Test method existance
+test("All required methods exist", function() {
+  ok(Character.prototype.getLevel, "getLevel method exists");
+  ok(Character.prototype.setLevel, "setLevel method exists");
+});
+
+// Test that methods are providing the correct values
+test("All methods provide the correct type of value", function() {
+  strictEqual($.type(Character.prototype.getLevel()), "number", "getLevel result is a number");
+  strictEqual($.type(Character.prototype.setLevel(2)), "number", "setLevel result is a number");
 });
